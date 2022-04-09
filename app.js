87102ef67beb658f0379d6d7517083293ad3e4f7
Rehaan99 <rehaanmp@hotmail.co.uk>
2022-04-09 03:25:39 +0100

    Save completed state of task and display it on the page whilst also maintaining order

--- app.js
+++ app.js
@@ -5,29 +5,43 @@ const filterOption = document.querySelector(".filter-todo");
 
 document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
-todoList.addEventListener("click", deleteCheck);
+todoList.addEventListener("click", taskButtonClickCheck);
 filterOption.addEventListener("change", filterTodo);
 
 function getTodos() {
   const todos = localStorageTasks();
-  todos.forEach(({ task: todo }) => {
+  sortList(todos);
+  todos.forEach((todo) => {
     addTaskToPage(todo);
   });
+}
+
+function sortList(todos) {
+  //bubble sort
+  for (i = 0; i < todos.length; i++) {
+    for (j = 0; j < todos.length - 1; j++) {
+      if (todos[j].id > todos[j + 1].id) {
+        const index = todos[j];
+        todos[j] = todos[j + 1];
+        todos[j + 1] = index;
+      }
+    }
+  }
 }
 
 function addTodo(event) {
   const taskList = {
     task: todoInput.value,
-    id: 1,
+    id: null,
     isComplete: false,
     wontDo: false,
   };
   saveLocalTodos(taskList);
   event.preventDefault();
-  addTaskToPage(taskList.task);
+  addTaskToPage(taskList);
 }
 
-function addTaskToPage(todo) {
+function addTaskToPage({ task: todo, isComplete: complete }) {
   const todoDiv = document.createElement("div"),
     newTodo = document.createElement("li"),
     completedButton = document.createElement("button"),
@@ -44,11 +58,17 @@ function addTaskToPage(todo) {
   trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
   todoList.appendChild(todoDiv);
+  if (complete) {
+    todoDiv.classList.toggle("completed");
+  }
   todoInput.value = "";
 }
 
 function saveLocalTodos(todo) {
   const todos = localStorageTasks();
+  if (todo.id === null) {
+    todo.id = todos.length;
+  }
   todos.push(todo);
   localStorage.setItem("todos", JSON.stringify(todos));
 }
@@ -60,18 +80,25 @@ function localStorageTasks() {
       : JSON.parse(localStorage.getItem("todos")));
 }
 
-function deleteCheck(event) {
+function taskButtonClickCheck(event) {
   const item = event.target;
+  const todo = item.parentElement;
+
   if (item.classList[0] === "trash-btn") {
-    const todo = item.parentElement;
     todo.classList.add("fall");
     removeLocalToDos(todo);
     todo.addEventListener("transitionend", function () {
       todo.remove();
     });
   } else if (item.classList[0] === "complete-btn") {
-    const todo = item.parentElement;
     todo.classList.toggle("completed");
+    const todos = localStorageTasks();
+    const filteredTodos = todos.filter(function (task) {
+      return task.task === todo.children[0].innerText;
+    });
+    filteredTodos[0].isComplete = !filteredTodos[0].isComplete;
+    removeLocalToDos(todo);
+    saveLocalTodos(filteredTodos[0]);
   }
 }
 
